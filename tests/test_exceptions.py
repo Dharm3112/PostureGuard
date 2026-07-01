7ae079d8d4cd5e3fc3daf0a441328875ef1c4525
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-01 21:48:09 +0530

    Commit 152: Add unit test check for InvalidLogBytesError in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,5 @@
 import unittest
-from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError
+from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError, InvalidLogBytesError
 
 class TestExceptions(unittest.TestCase):
     def test_exception_properties(self) -> None:
@@ -17,6 +17,10 @@ class TestExceptions(unittest.TestCase):
         save_exc = ConfigurationSaveError("Save failed")
         self.assertEqual(save_exc.error_code, "ConfigurationSaveError")
         self.assertEqual(save_exc.message, "Save failed")
+
+        exc_invalidlogbyteserror = InvalidLogBytesError("Test msg")
+        self.assertEqual(exc_invalidlogbyteserror.error_code, "InvalidLogBytesError")
+        self.assertEqual(exc_invalidlogbyteserror.message, "Test msg")
 
         exc_invalidalertframeserror = InvalidAlertFramesError("Test msg")
         self.assertEqual(exc_invalidalertframeserror.error_code, "InvalidAlertFramesError")
