bce3a7685a518a474d3003106d65bf9db28e04a1
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-01 21:48:57 +0530

    Commit 188: Add scale factor frame step settings verification comment in posture_detector.py

--- posture_detector.py
+++ posture_detector.py
@@ -34,7 +34,7 @@ class PostureDetector:
         else:
             self.logger.info("OpenCV Haar cascade classifier loaded successfully.")
 
-        self.scale_factor = scale_factor
+        self.scale_factor = scale_factor  # Scale settings for frame size step checks
         self.min_neighbors = min_neighbors
         self.consecutive_failures = 0
 
