1af876ef6250fa7754000828f6a9dda98ed8e348
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-01 21:47:34 +0530

    Commit 148: Add unit test check for InvalidResolutionError in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,5 @@
 import unittest
-from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError
+from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError
 
 class TestExceptions(unittest.TestCase):
     def test_exception_properties(self) -> None:
@@ -17,6 +17,10 @@ class TestExceptions(unittest.TestCase):
         save_exc = ConfigurationSaveError("Save failed")
         self.assertEqual(save_exc.error_code, "ConfigurationSaveError")
         self.assertEqual(save_exc.message, "Save failed")
+
+        exc_invalidresolutionerror = InvalidResolutionError("Test msg")
+        self.assertEqual(exc_invalidresolutionerror.error_code, "InvalidResolutionError")
+        self.assertEqual(exc_invalidresolutionerror.message, "Test msg")
 
 if __name__ == "__main__":
     unittest.main()
