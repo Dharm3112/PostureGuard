6037be30d8ca680f26000c0b337890460b9b822c
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-04 20:17:38 +0530

    Commit 260: Add unit test check for StatisticsWindowError in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,5 @@
 import unittest
-from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError, InvalidLogBytesError, InvalidBackupCountError, InvalidConfigFormatError, ConfigFileNotFoundError, ConfigWritePermissionError, StatsFileWriteError, StatsFileReadError, HistoryFileNotFoundError, GUIWindowInitializationError, WidgetBindingError, ThemeLoadingError, TooltipConfigurationError, SettingsDialogError
+from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError, InvalidLogBytesError, InvalidBackupCountError, InvalidConfigFormatError, ConfigFileNotFoundError, ConfigWritePermissionError, StatsFileWriteError, StatsFileReadError, HistoryFileNotFoundError, GUIWindowInitializationError, WidgetBindingError, ThemeLoadingError, TooltipConfigurationError, SettingsDialogError, StatisticsWindowError
 
 class TestExceptions(unittest.TestCase):
     def test_exception_properties(self) -> None:
@@ -18,6 +18,10 @@ class TestExceptions(unittest.TestCase):
         save_exc = ConfigurationSaveError("Save failed")
         self.assertEqual(save_exc.error_code, "ConfigurationSaveError")
         self.assertEqual(save_exc.message, "Save failed")
+
+        exc_statisticswindowerror = StatisticsWindowError("Test msg")
+        self.assertEqual(exc_statisticswindowerror.error_code, "StatisticsWindowError")
+        self.assertEqual(exc_statisticswindowerror.message, "Test msg")
 
         exc_settingsdialogerror = SettingsDialogError("Test msg")
         self.assertEqual(exc_settingsdialogerror.error_code, "SettingsDialogError")
