7ed01f067928ab64ecd4ee9485036feda2615f79
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-04 20:17:27 +0530

    Commit 257: Add unit test check for ThemeLoadingError in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,5 @@
 import unittest
-from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError, InvalidLogBytesError, InvalidBackupCountError, InvalidConfigFormatError, ConfigFileNotFoundError, ConfigWritePermissionError, StatsFileWriteError, StatsFileReadError, HistoryFileNotFoundError, GUIWindowInitializationError, WidgetBindingError
+from exceptions import PostureGuardException, CameraNotFoundError, ModelLoadError, LoggingConfigurationError, ConfigurationSaveError, InvalidResolutionError, InvalidThresholdError, InvalidDelayError, InvalidAlertFramesError, InvalidLogBytesError, InvalidBackupCountError, InvalidConfigFormatError, ConfigFileNotFoundError, ConfigWritePermissionError, StatsFileWriteError, StatsFileReadError, HistoryFileNotFoundError, GUIWindowInitializationError, WidgetBindingError, ThemeLoadingError
 
 class TestExceptions(unittest.TestCase):
     def test_exception_properties(self) -> None:
@@ -18,6 +18,10 @@ class TestExceptions(unittest.TestCase):
         save_exc = ConfigurationSaveError("Save failed")
         self.assertEqual(save_exc.error_code, "ConfigurationSaveError")
         self.assertEqual(save_exc.message, "Save failed")
+
+        exc_themeloadingerror = ThemeLoadingError("Test msg")
+        self.assertEqual(exc_themeloadingerror.error_code, "ThemeLoadingError")
+        self.assertEqual(exc_themeloadingerror.message, "Test msg")
 
         exc_widgetbindingerror = WidgetBindingError("Test msg")
         self.assertEqual(exc_widgetbindingerror.error_code, "WidgetBindingError")
