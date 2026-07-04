ac68397705a5c368a7619577b8ec071cd832deee
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-04 20:16:48 +0530

    Commit 245: Add ThemeLoadingError exception to exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -92,6 +92,10 @@ class WidgetBindingError(PostureGuardException):
     """Exception raised when layout event bindings initialization fails."""
     pass
 
+class ThemeLoadingError(PostureGuardException):
+    """Exception raised when visual custom theme properties lookup fails."""
+    pass
+
 class ConfigurationSaveError(PostureGuardException):
     """Exception raised when configuration file writing fails due to permission or OS errors."""
     pass
