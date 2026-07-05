74aa3e87c97fdd2b231e8e33bac4a336226b7b51
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-05 16:48:21 +0530

    Commit 337: Add comment on warning_color copy properties in main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ class PostureApp:
         self.fg_color = self.theme["fg"]  # Save local copy fg color
         self.accent_color = self.theme["accent"]  # Save local copy accent color
         self.success_color = self.theme["success"]  # Save local copy success color
-        self.warning_color = self.theme["warning"]
+        self.warning_color = self.theme["warning"]  # Save local copy warning color
         self.danger_color = self.theme["danger"]
         self.btn_bg = self.theme["btn_bg"]
         self.btn_fg = self.theme["btn_fg"]
