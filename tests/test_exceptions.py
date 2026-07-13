6a528f974e43cc5d0b16f32e3ef2b22d1812d8ff
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-13 21:42:15 +0530

    Commit 37: Add unit test check verifying FontLoadingError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -277,5 +277,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UIElementNotFoundError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_fontloadingerror(self) -> None:
+        from exceptions import FontLoadingError
+        exc = FontLoadingError("Test msg")
+        self.assertEqual(exc.error_code, "FontLoadingError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
