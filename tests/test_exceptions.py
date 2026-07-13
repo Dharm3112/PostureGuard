34127a82ffdbbc65eb6e24e04a6a8f2bc210a31c
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-13 21:41:34 +0530

    Commit 27: Add unit test check verifying LoggerInitializationError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -217,5 +217,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ConfigFileLockError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_loggerinitializationerror(self) -> None:
+        from exceptions import LoggerInitializationError
+        exc = LoggerInitializationError("Test msg")
+        self.assertEqual(exc.error_code, "LoggerInitializationError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
