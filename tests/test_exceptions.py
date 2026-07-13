17fb51264f2e874475fbdac6e20e00f2711cebad
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-13 21:42:37 +0530

    Commit 42: Add unit test check verifying ThreadTerminationError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -307,5 +307,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ThreadStartTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_threadterminationerror(self) -> None:
+        from exceptions import ThreadTerminationError
+        exc = ThreadTerminationError("Test msg")
+        self.assertEqual(exc.error_code, "ThreadTerminationError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
