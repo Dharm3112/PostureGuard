3a4244e4b977cb591bb3de5fee922b4d3992e058
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-13 21:41:38 +0530

    Commit 28: Add unit test check verifying LogFileOpenError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -223,5 +223,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LoggerInitializationError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_logfileopenerror(self) -> None:
+        from exceptions import LogFileOpenError
+        exc = LogFileOpenError("Test msg")
+        self.assertEqual(exc.error_code, "LogFileOpenError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
