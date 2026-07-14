58271eaf1bdf2e2caeb461a78b8ae74ba08e4e49
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-14 21:34:10 +0530

    Commit 28: Add unit test check verifying CSVTempFileRenameError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -403,5 +403,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVCloseFileError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_csvtempfilerenameerror(self) -> None:
+        from exceptions import CSVTempFileRenameError
+        exc = CSVTempFileRenameError("Test msg")
+        self.assertEqual(exc.error_code, "CSVTempFileRenameError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
