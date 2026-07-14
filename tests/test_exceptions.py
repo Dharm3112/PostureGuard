4f051dc542026baa0f18e458ffc36b1ae456e499
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-14 21:34:14 +0530

    Commit 29: Add unit test check verifying UIWidgetGeometryError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -409,5 +409,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVTempFileRenameError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_uiwidgetgeometryerror(self) -> None:
+        from exceptions import UIWidgetGeometryError
+        exc = UIWidgetGeometryError("Test msg")
+        self.assertEqual(exc.error_code, "UIWidgetGeometryError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
