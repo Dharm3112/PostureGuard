898b8ea1dec96e8b29104d114593ce600d5fb9d4
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-17 22:55:17 +0530

    Commit 31: Add unit test check verifying LogFileWriteLockError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -511,5 +511,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ConfigLockTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_logfilewritelockerror(self) -> None:
+        from exceptions import LogFileWriteLockError
+        exc = LogFileWriteLockError("Test msg")
+        self.assertEqual(exc.error_code, "LogFileWriteLockError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
