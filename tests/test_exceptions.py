90a3ad0100fc66d5fddf398ae59c4a2ef230db57
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-17 22:56:11 +0530

    Commit 50: Add unit test check verifying AudioPlaybackDeviceNotFoundError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -625,5 +625,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CalibrateBaselineOutOfBoundsError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_audioplaybackdevicenotfounderror(self) -> None:
+        from exceptions import AudioPlaybackDeviceNotFoundError
+        exc = AudioPlaybackDeviceNotFoundError("Test msg")
+        self.assertEqual(exc.error_code, "AudioPlaybackDeviceNotFoundError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
