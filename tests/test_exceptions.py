60fe0811804b87c27ef9221870ee9e7eb0ae3a1f
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-17 22:55:19 +0530

    Commit 32: Add unit test check verifying LogHandlerCleanupError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -517,5 +517,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogFileWriteLockError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_loghandlercleanuperror(self) -> None:
+        from exceptions import LogHandlerCleanupError
+        exc = LogHandlerCleanupError("Test msg")
+        self.assertEqual(exc.error_code, "LogHandlerCleanupError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
