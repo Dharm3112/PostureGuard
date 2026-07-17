b3f2f4c5f2200274a230aff9fdd7804830705d3a
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-17 22:55:59 +0530

    Commit 45: Add unit test check verifying CameraBufferEmptyError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -595,5 +595,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CameraResolutionHardwareMismatchError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_camerabufferemptyerror(self) -> None:
+        from exceptions import CameraBufferEmptyError
+        exc = CameraBufferEmptyError("Test msg")
+        self.assertEqual(exc.error_code, "CameraBufferEmptyError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
