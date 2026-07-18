d433c47764654091470f0fd12c2cb2a7204ffcbc
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-18 19:45:36 +0530

    Commit 28: Add unit test check verifying LogConsoleColorError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -673,5 +673,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogHandlerFlushError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_logconsolecolorerror(self) -> None:
+        from exceptions import LogConsoleColorError
+        exc = LogConsoleColorError("Test msg")
+        self.assertEqual(exc.error_code, "LogConsoleColorError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
