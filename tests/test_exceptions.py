af6e751c3d00c7860246d6282e9edd8d118c637d
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-18 19:45:39 +0530

    Commit 30: Add unit test check verifying CSVFieldMismatchError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -685,5 +685,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVLineReadError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_csvfieldmismatcherror(self) -> None:
+        from exceptions import CSVFieldMismatchError
+        exc = CSVFieldMismatchError("Test msg")
+        self.assertEqual(exc.error_code, "CSVFieldMismatchError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
