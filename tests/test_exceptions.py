29076f918e80e3a5304373eabb93ee47f7bea6d3
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-21 11:44:26 +0530

    Commit 32: Add unit test check verifying LogHandlerRegistrationError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -787,5 +787,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ConfigReadOnlyError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_loghandlerregistrationerror(self) -> None:
+        from exceptions import LogHandlerRegistrationError
+        exc = LogHandlerRegistrationError("Test msg")
+        self.assertEqual(exc.error_code, "LogHandlerRegistrationError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
