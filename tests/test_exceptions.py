ab995883c931ebdf19cb88478680a13506bad00a
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-21 11:44:51 +0530

    Commit 50: Add unit test check verifying CameraUnsupportedResolutionError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -895,5 +895,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "FrameBufferReadTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_cameraunsupportedresolutionerror(self) -> None:
+        from exceptions import CameraUnsupportedResolutionError
+        exc = CameraUnsupportedResolutionError("Test msg")
+        self.assertEqual(exc.error_code, "CameraUnsupportedResolutionError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
