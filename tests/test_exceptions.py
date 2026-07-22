1ea5781a91c287f81241d500ccb0dfe14bd940a0
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-22 21:29:12 +0530

    Commit 37: Add unit test check verifying FaceDetectionBoundingBoxError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -997,5 +997,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ModelCascadeReloadError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_facedetectionboundingboxerror(self) -> None:
+        from exceptions import FaceDetectionBoundingBoxError
+        exc = FaceDetectionBoundingBoxError("Test msg")
+        self.assertEqual(exc.error_code, "FaceDetectionBoundingBoxError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
