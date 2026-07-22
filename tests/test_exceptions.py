93d2ef22ff2710c1bc9a5a78e2f42d3f8616c2a5
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-22 21:29:15 +0530

    Commit 39: Add unit test check verifying StatsExportFormatError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1009,5 +1009,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CalibrateBaselineJitterError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_statsexportformaterror(self) -> None:
+        from exceptions import StatsExportFormatError
+        exc = StatsExportFormatError("Test msg")
+        self.assertEqual(exc.error_code, "StatsExportFormatError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
