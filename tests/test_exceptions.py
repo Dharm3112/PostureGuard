68c34767df75b99c3e6b7019bd9e6b0b847563ae
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-24 22:02:16 +0530

    Commit 30: Add unit test check verifying CSVRecordFormatError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1075,5 +1075,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVColumnIndexError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_csvrecordformaterror(self) -> None:
+        from exceptions import CSVRecordFormatError
+        exc = CSVRecordFormatError("Test msg")
+        self.assertEqual(exc.error_code, "CSVRecordFormatError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
