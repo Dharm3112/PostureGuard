4b2046ba34f32a9d96690735e1a9ab0c5011b451
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-24 22:02:04 +0530

    Commit 24: Add unit test check verifying NotificationIconLoadError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1039,5 +1039,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "NotificationServiceTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_notificationiconloaderror(self) -> None:
+        from exceptions import NotificationIconLoadError
+        exc = NotificationIconLoadError("Test msg")
+        self.assertEqual(exc.error_code, "NotificationIconLoadError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
