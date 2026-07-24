0390c8c1e9f302cff67b252a43f533b9be464f14
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-24 22:02:25 +0530

    Commit 34: Add unit test check verifying CameraPropertySetError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1099,5 +1099,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UIEventLoopTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_camerapropertyseterror(self) -> None:
+        from exceptions import CameraPropertySetError
+        exc = CameraPropertySetError("Test msg")
+        self.assertEqual(exc.error_code, "CameraPropertySetError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
