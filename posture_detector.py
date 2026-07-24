62852a4cf0df545a16d6a96190bae8c81be55307
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-24 22:03:10 +0530

    Commit 54: Add inline comment clarifying detector initialization log in posture_detector.py

--- posture_detector.py
+++ posture_detector.py
@@ -20,7 +20,7 @@ class PostureDetector:
         :param buffer_size: Number of frames to average for smoothing vertical movement jitter.
         """
         self.logger = setup_logger()  # Bind logger output streams
-        self.logger.info("Initializing PostureDetector...")
+        self.logger.info("Initializing PostureDetector...")  # Log detector initialization start
 
         # Setup localized cascade file parameters from libraries tracker loader
         # Verify path config integrity check
