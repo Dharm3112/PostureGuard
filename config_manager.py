226772ca7e63c70dd326c4732fb89b0734d1f516
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-25 19:55:15 +0530

    Commit 41: Add inline comment clarifying CAMERA_WIDTH key identifier in config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -17,7 +17,7 @@ class ConfigManager:
     SLOUCH_THRESHOLD = "slouch_threshold_px"  # Key identifier for slouch threshold setting
     TIME_TO_ALERT = "time_to_alert_frames"  # Key identifier for time to alert frames setting
     FRAME_DELAY = "frame_delay_ms"
-    CAMERA_WIDTH = "camera_width"
+    CAMERA_WIDTH = "camera_width"  # Key identifier for camera width setting
     CAMERA_HEIGHT = "camera_height"
     SAVE_HISTORY = "save_history"
     SAVED_BASELINE_Y = "saved_baseline_y"
