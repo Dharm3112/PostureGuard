58eab611280b4b7c8841ed840f86e6d4b5e82d34
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-25 19:55:05 +0530

    Commit 35: Add unit test check verifying CameraFrameFormatError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1225,5 +1225,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CameraResolutionUnsupportedError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_cameraframeformaterror(self) -> None:
+        from exceptions import CameraFrameFormatError
+        exc = CameraFrameFormatError("Test msg")
+        self.assertEqual(exc.error_code, "CameraFrameFormatError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
