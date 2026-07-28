b92944329c71fdcf754b71cd07cc93e9bd008ed2
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-28 20:39:32 +0530

    Commit 26: Add unit test check verifying AudioDeviceStreamError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1261,5 +1261,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "StatsCalculationFloatError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_audiodevicestreamerror(self) -> None:
+        from exceptions import AudioDeviceStreamError
+        exc = AudioDeviceStreamError("Test msg")
+        self.assertEqual(exc.error_code, "AudioDeviceStreamError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
