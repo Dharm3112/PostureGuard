198d73e27f29a765b563bcef75f66e3fa39ceaf7
Dharm Patel <dharmpatel311287@gmail.com>
2026-07-30 21:30:44 +0530

    Commit 23: Add unit test check verifying UIWidgetHierarchyError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1471,5 +1471,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVTimestampParseFormatError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_uiwidgethierarchyerror(self) -> None:
+        from exceptions import UIWidgetHierarchyError
+        exc = UIWidgetHierarchyError("Test msg")
+        self.assertEqual(exc.error_code, "UIWidgetHierarchyError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
