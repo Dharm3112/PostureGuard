f3fa0a7f6b1583fa5ea900b7adbb33112e29f2fc
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-02 17:46:08 +0530

    Commit 34: Add unit test check verifying CameraFPSSettingError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1561,5 +1561,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UIWindowResizeLimitError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_camerafpssettingerror(self) -> None:
+        from exceptions import CameraFPSSettingError
+        exc = CameraFPSSettingError("Test msg")
+        self.assertEqual(exc.error_code, "CameraFPSSettingError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
