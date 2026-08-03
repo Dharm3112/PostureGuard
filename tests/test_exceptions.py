1b847d413b066412054d51d830860194c1f7ed20
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-03 19:11:14 +0530

    Commit 19: Add unit test check verifying NotificationPriorityError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1615,5 +1615,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "AudioSampleChannelMismatchError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_notificationpriorityerror(self) -> None:
+        from exceptions import NotificationPriorityError
+        exc = NotificationPriorityError("Test msg")
+        self.assertEqual(exc.error_code, "NotificationPriorityError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
