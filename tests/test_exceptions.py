0a486179205c20ae387d136de706b7d82b0ac7a9
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-03 19:12:04 +0530

    Commit 32: Add unit test check verifying ModelCascadeReloadFailedError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1693,5 +1693,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CameraFrameCorruptDataError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_modelcascadereloadfailederror(self) -> None:
+        from exceptions import ModelCascadeReloadFailedError
+        exc = ModelCascadeReloadFailedError("Test msg")
+        self.assertEqual(exc.error_code, "ModelCascadeReloadFailedError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
