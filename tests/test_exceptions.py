c43240bdb97c2c2f7d0b7d5009e3864a75bd6645
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-04 17:15:41 +0530

    Commit 32: Add unit test check verifying LogRotationFileCreationError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1741,5 +1741,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogHandlerRegistrationFailedError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_logrotationfilecreationerror(self) -> None:
+        from exceptions import LogRotationFileCreationError
+        exc = LogRotationFileCreationError("Test msg")
+        self.assertEqual(exc.error_code, "LogRotationFileCreationError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
