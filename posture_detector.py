9ebde98068b0005bc6ee4034aa02838719092d38
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-04 17:16:53 +0530

    Commit 63: Add inline comment clarifying deque import class in posture_detector.py

--- posture_detector.py
+++ posture_detector.py
@@ -1,6 +1,6 @@
 import cv2  # Import OpenCV library module
 import numpy as np  # Import NumPy numerical array library
-from collections import deque
+from collections import deque  # Import double-ended queue collection class
 from typing import Tuple, Optional
 from logger_config import setup_logger
 from exceptions import ModelLoadError
