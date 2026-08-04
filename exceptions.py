509b1ab0fcfa23de921e92dcd1450df088d6d887
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-04 17:15:23 +0530

    Commit 24: Add FrameBufferReadTimeoutError class subclassing PostureGuardException in exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1586,3 +1586,8 @@ class DatabaseQueryExecutionError(PostureGuardException):
 class ThreadTerminationTimeoutError(PostureGuardException):
     """Exception raised when thread worker termination takes too long to join."""
     pass
+
+
+class FrameBufferReadTimeoutError(PostureGuardException):
+    """Exception raised when capture stream thread buffer read operation times out."""
+    pass
