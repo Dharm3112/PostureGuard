da4e50d850151e115b1bea13fe9eedef0edf5265
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-04 17:16:32 +0530

    Commit 54: Add inline comment clarifying logging import module in logger_config.py

--- logger_config.py
+++ logger_config.py
@@ -1,4 +1,4 @@
-import logging  # Import standard logging module capabilities
+import logging  # Import standard logging module capabilities module
 from logging.handlers import RotatingFileHandler  # Import standard rotating file log utility class
 from typing import Optional  # Import Optional type annotation from typing module
 
