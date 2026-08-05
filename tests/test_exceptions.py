8fca63f17fc160713b4be28c5b49e07a496ae24e
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-05 21:39:47 +0530

    Commit 20: Add unit test check verifying CSVColumnHeaderFormatError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1891,5 +1891,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogRotationDiskSpaceError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_csvcolumnheaderformaterror(self) -> None:
+        from exceptions import CSVColumnHeaderFormatError
+        exc = CSVColumnHeaderFormatError("Test msg")
+        self.assertEqual(exc.error_code, "CSVColumnHeaderFormatError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
