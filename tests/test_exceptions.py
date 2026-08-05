2baa65957a4e882055f42dcd083cfbb72e5d734b
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-05 21:39:33 +0530

    Commit 12: Add unit test check verifying AudioPlaybackEngineError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1843,5 +1843,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "FrameBufferReadTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_audioplaybackengineerror(self) -> None:
+        from exceptions import AudioPlaybackEngineError
+        exc = AudioPlaybackEngineError("Test msg")
+        self.assertEqual(exc.error_code, "AudioPlaybackEngineError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
