7e4f59d723ce9823c336367bdaa68e8896b92252
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-07 22:19:09 +0530

    Commit 34: Add unit test check verifying CameraDeviceBusyAccessError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1987,5 +1987,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UICanvasRenderPipelineError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_cameradevicebusyaccesserror(self) -> None:
+        from exceptions import CameraDeviceBusyAccessError
+        exc = CameraDeviceBusyAccessError("Test msg")
+        self.assertEqual(exc.error_code, "CameraDeviceBusyAccessError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
