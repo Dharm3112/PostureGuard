8d7d3e5f963ef26ccda99f12dd9cc0faaeee5ee7
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-09 16:41:14 +0530

    Commit 31: Add inline comment clarifying FRAME_DELAY in config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -16,7 +16,7 @@ class ConfigManager:
     CAMERA_INDEX = "camera_index"  # Key identifier for webcam index setting item value key
     SLOUCH_THRESHOLD = "slouch_threshold_px"  # Key identifier for slouch threshold setting item value
     TIME_TO_ALERT = "time_to_alert_frames"  # Key identifier for time to alert frames setting item value
-    FRAME_DELAY = "frame_delay_ms"
+    FRAME_DELAY = "frame_delay_ms"  # Key identifier for frame delay setting item
     CAMERA_WIDTH = "camera_width"  # Key identifier for camera width setting
     CAMERA_HEIGHT = "camera_height"  # Key identifier for camera height setting
     SAVE_HISTORY = "save_history"  # Key identifier for history logging setting
