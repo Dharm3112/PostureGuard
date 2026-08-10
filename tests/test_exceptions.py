ff4fb718e2826503e0cb5b5555454aa5dd6d2698
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-10 20:00:03 +0530

    Commit 34: Add unit test check verifying ModelCascadeFileAccessError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2203,5 +2203,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CameraResolutionConstraintError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_modelcascadefileaccesserror(self) -> None:
+        from exceptions import ModelCascadeFileAccessError
+        exc = ModelCascadeFileAccessError("Test msg")
+        self.assertEqual(exc.error_code, "ModelCascadeFileAccessError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
