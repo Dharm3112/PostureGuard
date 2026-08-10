db5265e6f97d3ccb9b42c8aa2ee3dd3f71ff9267
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-10 20:00:08 +0530

    Commit 37: Add unit test check verifying StatsGraphExportFormatError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2221,5 +2221,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CalibrateBaselineMinFramesError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_statsgraphexportformaterror(self) -> None:
+        from exceptions import StatsGraphExportFormatError
+        exc = StatsGraphExportFormatError("Test msg")
+        self.assertEqual(exc.error_code, "StatsGraphExportFormatError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
