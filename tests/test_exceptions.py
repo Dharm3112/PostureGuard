9f29fbbcf9e1b7bc17c43db34954615724351807
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-15 20:24:22 +0530

    Commit 39: Add unit test check verifying CalibrateBaselineBufferEmptyError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2509,5 +2509,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "FaceDetectionROISizeZeroError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_calibratebaselinebufferemptyerror(self) -> None:
+        from exceptions import CalibrateBaselineBufferEmptyError
+        exc = CalibrateBaselineBufferEmptyError("Test msg")
+        self.assertEqual(exc.error_code, "CalibrateBaselineBufferEmptyError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
