ebaa6360059f6359a93af66048ea9607056f0580
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-17 22:56:37 +0530

    Commit 19: Add unit test check verifying CSVRecordMissingTimestampFieldError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2593,5 +2593,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogRotationArchiveIndexOverflowError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_csvrecordmissingtimestampfielderror(self) -> None:
+        from exceptions import CSVRecordMissingTimestampFieldError
+        exc = CSVRecordMissingTimestampFieldError("Test msg")
+        self.assertEqual(exc.error_code, "CSVRecordMissingTimestampFieldError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
