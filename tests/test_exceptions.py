548c8f381e93f901ff593fb8c1e299bd06678abb
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-17 22:56:31 +0530

    Commit 15: Add unit test check verifying ConfigKeyValidationTypeMismatchError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2569,5 +2569,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "NotificationIconResourceSizeZeroError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_configkeyvalidationtypemismatcherror(self) -> None:
+        from exceptions import ConfigKeyValidationTypeMismatchError
+        exc = ConfigKeyValidationTypeMismatchError("Test msg")
+        self.assertEqual(exc.error_code, "ConfigKeyValidationTypeMismatchError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
