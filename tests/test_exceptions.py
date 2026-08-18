0cc7666b8f31978e84c8c754bac1859ccc126b6a
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-18 20:33:20 +0530

    Commit 16: Add unit test check verifying NotificationChannelRegistrationTimeoutError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2617,5 +2617,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "AudioSampleRateUnsupportedError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_notificationchannelregistrationtimeouterror(self) -> None:
+        from exceptions import NotificationChannelRegistrationTimeoutError
+        exc = NotificationChannelRegistrationTimeoutError("Test msg")
+        self.assertEqual(exc.error_code, "NotificationChannelRegistrationTimeoutError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
