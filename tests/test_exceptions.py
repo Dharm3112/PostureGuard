fb5fe1c6f2272de47a139bfa58d34b2e9ef94c97
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-20 19:57:52 +0530

    Commit 30: Add unit test check verifying CameraStreamBufferReadTimeoutError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2755,5 +2755,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UIThemePaletteColorFormatError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_camerastreambufferreadtimeouterror(self) -> None:
+        from exceptions import CameraStreamBufferReadTimeoutError
+        exc = CameraStreamBufferReadTimeoutError("Test msg")
+        self.assertEqual(exc.error_code, "CameraStreamBufferReadTimeoutError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
