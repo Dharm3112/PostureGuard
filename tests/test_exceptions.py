95014ee85833780bc30a0496c8c1a15612213ce1
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-20 19:57:55 +0530

    Commit 32: Add unit test check verifying ModelCascadeXmlSchemaValidationError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2767,5 +2767,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CameraDeviceConfigurationConstraintError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_modelcascadexmlschemavalidationerror(self) -> None:
+        from exceptions import ModelCascadeXmlSchemaValidationError
+        exc = ModelCascadeXmlSchemaValidationError("Test msg")
+        self.assertEqual(exc.error_code, "ModelCascadeXmlSchemaValidationError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
