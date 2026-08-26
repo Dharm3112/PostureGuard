7a1fac4dd87dec0747b3314a9ce877af0fef56a7
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-26 22:36:08 +0530

    Commit 56: Add inline comment clarifying scale_factor param in posture_detector.py

--- posture_detector.py
+++ posture_detector.py
@@ -34,7 +34,7 @@ class PostureDetector:
         else:
             self.logger.info("OpenCV Haar cascade classifier loaded successfully.")  # Log success message
 
-        self.scale_factor = scale_factor  # Set resolution sizing scale scaling parameters values filter configuration setting value setting
+        self.scale_factor = scale_factor  # Set resolution sizing scale scaling parameters values filter configuration setting value setting param
         self.min_neighbors = min_neighbors  # Set detection threshold neighbor count filters bounds range value setting value setting
         self.consecutive_failures = 0  # Initialize consecutive detection failure counters check limits value setting value setting
 
