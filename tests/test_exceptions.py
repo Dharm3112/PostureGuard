2ffeb594a24fcf5c00b039234105cb88ed4b050a
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-26 22:34:47 +0530

    Commit 29: Add unit test check verifying LogRotationArchiveStorageFullError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2887,5 +2887,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "LogHandlerStreamFlushWriteTimeoutError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_logrotationarchivestoragefullerror(self) -> None:
+        from exceptions import LogRotationArchiveStorageFullError
+        exc = LogRotationArchiveStorageFullError("Test msg")
+        self.assertEqual(exc.error_code, "LogRotationArchiveStorageFullError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
