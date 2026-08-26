338f625827cee9eef878f30381bf58d4e6cddd62
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-26 22:34:26 +0530

    Commit 22: Add unit test check verifying AudioOutputDeviceStreamCloseError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2845,5 +2845,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "UIWidgetGeometryBoundsInvalidError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_audiooutputdevicestreamcloseerror(self) -> None:
+        from exceptions import AudioOutputDeviceStreamCloseError
+        exc = AudioOutputDeviceStreamCloseError("Test msg")
+        self.assertEqual(exc.error_code, "AudioOutputDeviceStreamCloseError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
