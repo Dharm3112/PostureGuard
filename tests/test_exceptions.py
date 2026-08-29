a739560f639c4b9d8b05fc168b1f9767da2e9322
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-29 16:29:07 +0530

    Commit 21: Add unit test check verifying NotificationChannelCreationPermissionError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2983,5 +2983,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "AudioSampleRateDownsamplingError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_notificationchannelcreationpermissionerror(self) -> None:
+        from exceptions import NotificationChannelCreationPermissionError
+        exc = NotificationChannelCreationPermissionError("Test msg")
+        self.assertEqual(exc.error_code, "NotificationChannelCreationPermissionError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
