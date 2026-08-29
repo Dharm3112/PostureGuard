464c332267a6a42b8d44b577260b477e083a6311
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-29 16:29:05 +0530

    Commit 20: Add unit test check verifying AudioSampleRateDownsamplingError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2977,5 +2977,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "AudioOutputDeviceMuteStateError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_audiosampleratedownsamplingerror(self) -> None:
+        from exceptions import AudioSampleRateDownsamplingError
+        exc = AudioSampleRateDownsamplingError("Test msg")
+        self.assertEqual(exc.error_code, "AudioSampleRateDownsamplingError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
