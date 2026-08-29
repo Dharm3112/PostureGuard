866a3162ba1a29edfb0feae4a299729d3916fde4
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-29 16:29:14 +0530

    Commit 24: Add unit test check verifying ConfigMigrationVersionIncompatibleError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3001,5 +3001,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "ConfigKeyValidationNullValueDisallowedError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_configmigrationversionincompatibleerror(self) -> None:
+        from exceptions import ConfigMigrationVersionIncompatibleError
+        exc = ConfigMigrationVersionIncompatibleError("Test msg")
+        self.assertEqual(exc.error_code, "ConfigMigrationVersionIncompatibleError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
