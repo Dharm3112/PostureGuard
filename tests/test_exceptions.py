9f3aee36927c7ea63be33ce39204500b59829001
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-31 18:29:29 +0530

    Commit 24: Add unit test check verifying UIWidgetGeometryPackingConflictError mapping in test_exceptions.py

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3139,5 +3139,11 @@ class TestExceptions(unittest.TestCase):
         self.assertEqual(exc.error_code, "CSVTimestampDateFormatInvalidError")
         self.assertEqual(exc.message, "Test msg")
 
+    def test_uiwidgetgeometrypackingconflicterror(self) -> None:
+        from exceptions import UIWidgetGeometryPackingConflictError
+        exc = UIWidgetGeometryPackingConflictError("Test msg")
+        self.assertEqual(exc.error_code, "UIWidgetGeometryPackingConflictError")
+        self.assertEqual(exc.message, "Test msg")
+
 if __name__ == "__main__":
     unittest.main()
